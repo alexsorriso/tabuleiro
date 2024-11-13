97b64859eec9135970eb2d4a39fbaf7728d8be1c
Aluno <aluno@ceetepspg.local>
2024-11-12 21:37:13 -0300

    AAAAA

---
+++ src/Tabuleiro.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tabuleiro {
+
+    private ArrayList<Propriedade> propriedades;
+
+    Tabuleiro(String caminho){
+        propriedades= new ArrayList<Propriedade>();
+        Scanner scan = new Scanner(new FileReader(caminho));
+        scan.ne;
+    }
+}
